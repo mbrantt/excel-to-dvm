07210bd8290a0e9d3a306a3ea0eadda42c25a17b
mbrantt <abhi.andres@gmail.com>
2020-06-19 00:00:25 -0400

    fix - first row of data fetched by getAllRowDVMFormat will be removed

--- src/main/java/exceltodvm/controller/Convert.java
+++ src/main/java/exceltodvm/controller/Convert.java
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -15,7 +14,6 @@ import java.util.stream.StreamSupport;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -36,9 +34,12 @@ public class Convert {
 	    		sheetList.stream().map(rowList -> {
 	    			List<Cell> cellList= StreamSupport.stream(rowList.spliterator(), false).collect(Collectors.toList());
 	    			fila = new ArrayList<>();
-	    			fila.add("<row>");cellList.forEach(cell -> fila.add("<cell>"+cell+"</cell>"));fila.add("</row>");
+	    			fila.add("<row>");
+	    			cellList.forEach(cell -> fila.add("<cell>"+cell+"</cell>"));
+	    			fila.add("</row>");
 	    			dvm.setRow(fila);
-	    			return cellList.stream();}).collect(Collectors.toList());
+	    			return cellList.stream();
+	    		}).collect(Collectors.toList());
     			logger.info(dvm.getData().get(0));
 
 			}
@@ -54,24 +55,25 @@ public class Convert {
 	}
 	public StringBuilder getAllDocument() {
 		StringBuilder dvmDocument = new StringBuilder();
-		int lengthColumnHead = dvm.getData().get(0).stream().mapToInt(t -> t.length()).sum();
+		int lengthColumnHead = dvm.getData().get(0).stream().mapToInt(t -> t.length()+2).sum();
 		dvmDocument.append("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>").append(System.lineSeparator())
 			.append("<dvm name=\""+dvm.getNombre()+"\" xmlns=\"http://xmlns.oracle.com/dvm\">").append(System.lineSeparator())
 			.append("<description>"+dvm.getDescripcion()+"</description>").append(System.lineSeparator())
 			.append("<columns>").append(System.lineSeparator());
 		dvm.getData().get(0).stream()
 			.filter(cell -> cell.contains("<cell>"))
-			.forEach(columnName -> dvmDocument.append("<column name=\""+columnName.substring(6, columnName.length()-7)+"\"/>"));
+			.forEach(columnName -> dvmDocument.append("<column name=\""+columnName.substring(6, columnName.length()-7)+"\"/>").append(System.lineSeparator()));
 		dvmDocument.append("</columns>").append(System.lineSeparator())
-			.append("<rows>").append(getAllRowDVMFormat().delete(0, lengthColumnHead)).append("</rows>")
+			.append("<rows>").append(System.lineSeparator())
+			.append(getAllRowDVMFormat().delete(0, lengthColumnHead)).append("</rows>")
 			.append("</dvm>");
 		logger.info(dvmDocument);
 		return dvmDocument;
 	}
 	public void saveDocumentFile(File file) {
-		try(FileOutputStream fOut = new FileOutputStream(file)) {
-			fOut.write(getAllDocument().toString().getBytes());
-	        fOut.flush();
+		try(FileOutputStream fileOut = new FileOutputStream(file)) {
+			fileOut.write(getAllDocument().toString().getBytes());
+			fileOut.flush();
 		} catch (FileNotFoundException e) {
 			logger.error("File Error: %s", e);
 		} catch (IOException e) {
