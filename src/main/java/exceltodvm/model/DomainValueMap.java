3dfb6af8b07dbff60ec9ca07a91bbbba3fb78542
mbrantt <abhi.andres@gmail.com>
2020-06-20 19:13:54 -0400

    add line break to class

--- src/main/java/exceltodvm/model/DomainValueMap.java
+++ src/main/java/exceltodvm/model/DomainValueMap.java
@@ -5,15 +5,19 @@ import java.util.List;
 
 public class DomainValueMap extends Archive{
 	private List<ArrayList<String>> dataRowList;
+	
 	public DomainValueMap(String nameArchiveDVM) {
 		super(nameArchiveDVM);
 	}
+	
 	public DomainValueMap() {
 		super();
 	}
+	
 	public List<ArrayList<String>> getDataRowList() {
 		return dataRowList;
 	}
+	
 	public void setDataRowList(ArrayList<String> dataCell) {
 		this.dataRowList.add(dataCell);
 	}
